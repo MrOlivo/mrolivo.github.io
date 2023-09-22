41d819cd0f02d72ef38d20f8157ff839899f111d
Edwin Olivo <46498127+MrOlivo@users.noreply.github.com>
2023-09-22 08:29:56 -0700

    refactor: better icon naming

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,6 +1,7 @@
-export { default as bento } from "./bento.svg?raw";
-export { default as email } from "./email.svg?raw";
-export { default as github } from "./github.svg?raw";
-export { default as moon } from "./moon.svg?raw";
-export { default as sun } from "./sun.svg?raw";
-export { default as link } from "./link.svg?raw";
+export { default as BentoIcon } from "./bento.svg?raw";
+export { default as EmailIcon } from "./email.svg?raw";
+export { default as GithubIcon } from "./github.svg?raw";
+export { default as MoonIcon } from "./moon.svg?raw";
+export { default as SunIcon } from "./sun.svg?raw";
+export { default as LinkIcon } from "./link.svg?raw";
+export { default as MenuIcon } from "./hamburger.svg?raw";

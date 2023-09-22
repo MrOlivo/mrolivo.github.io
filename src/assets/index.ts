751d8f96f5f351fc90438b80200e97775d675a36
Edwin Olivo <46498127+MrOlivo@users.noreply.github.com>
2023-09-22 11:15:05 -0700

    refactor: svg icons have their own dir

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,7 +1,7 @@
-export { default as BentoIcon } from "./bento.svg?raw";
-export { default as EmailIcon } from "./email.svg?raw";
-export { default as GithubIcon } from "./github.svg?raw";
-export { default as MoonIcon } from "./moon.svg?raw";
-export { default as SunIcon } from "./sun.svg?raw";
-export { default as LinkIcon } from "./link.svg?raw";
-export { default as MenuIcon } from "./hamburger.svg?raw";
+export { default as BentoIcon } from "./svg/bento.svg?raw";
+export { default as EmailIcon } from "./svg/email.svg?raw";
+export { default as GithubIcon } from "./svg/github.svg?raw";
+export { default as MoonIcon } from "./svg/moon.svg?raw";
+export { default as SunIcon } from "./svg/sun.svg?raw";
+export { default as LinkIcon } from "./svg/link.svg?raw";
+export { default as MenuIcon } from "./svg/hamburger.svg?raw";
